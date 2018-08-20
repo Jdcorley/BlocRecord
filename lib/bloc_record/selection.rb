2923914e154e3eec1d3f05875c84c8863fe7a516
Jack <jdcorley@gmail.com>
2018-08-20 18:10:14 -0400

    select methods complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,23 +1,134 @@
 require 'sqlite3'
 
-module Selection 
-  def find(id) 
-    row = connection.get_first_row <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE id = #{id};
-    SQL
+module Selection
+  def find_some(*ids)
+   
+    if ids.length == 1
+      find_one(ids.first)
+    else 
+      ids.each do |id| 
+        if id < 0 
+          raise ArgumentError.new('ID invalid. ID must be a positive integer.')
+        end 
+      end 
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
+      SQL
 
-    data = HASH[columns.zip(row)]
-    new(data)
+      rows_to_array(rows)
+    end 
+  end 
+
+
+  def find_one(id)
+    if id < 0 
+      raise ArgumentError.new('ID invalid. ID must be a positive integer.')
+    else 
+      row = connection.get_first_row <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id = #{id};
+      SQL
+
+      init_object_from_row(row)
+    end 
   end 
 
   def find_by(attribute, value)
-    row = connection.get_first_row <<-SQL
+    rows = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
-      WHERE attribute = #{value};
+      WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
     SQL
 
-    data = ARRAY[columns.zip(row)]
-    new(data)
+    rows_to_array(rows)
+  end 
+
+  def method_missing(m, *args, &block)
+    if m == :find_by_name 
+      find_by(:name, *args[0])
+    end 
+  end  
+
+  def find_each(options={})
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{options[:batch_size]};
+    SQL
+     
+    row_to_array(rows).each do |row|
+      yield(row)
+    end 
+  end
+  
+  def find_in_batches(options={})
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{options[:batch_size]};
+    SQL
+
+    yield(rows_to_array(rows))
+  end 
+
+  def take_some(num=1)
+    if num > 1
+      rows = connection.execute <<-SQL 
+        SELECT #{columns.join ","} FROM #{table}
+        ORDER BY random()
+        LIMIT #{num};
+      SQL
+      
+      rows_to_array(rows)
+    else 
+      take_one 
+    end 
+  end 
+
+  def take_one 
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY random()
+      LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end 
+
+  def first 
+    row = connection.get_first_row <<-SQL 
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id ASC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end 
+
+  def last 
+    row = connection.get_first_row <<-SQL 
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id DESC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end 
+
+  def all 
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table};
+    SQL
+
+    rows_to_array(rows)
+  end 
+
+  private 
+
+  def init_object_from_row(row)
+    if row 
+      data = Hash[columns.zip(row)]
+      new(data)
+    end 
+  end 
+
+  def rows_to_array(rows)
+    rows.map { |row| new(Hash[columns.zip(row)]) }
   end 
 end 
